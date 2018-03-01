d35727ffc919ea057d7403d715de4ac592722c6f
Terence Ma <tma@gsma.com>
2018-03-01 15:56:47 +0800

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -122,6 +122,21 @@ app.post('/users', (req, res) =>{
 app.get('/users/me', authenticate, (req,res) =>{
     res.send(req.user);
 });
+
+// POST /users/login {email, password} Find a mongodb password
+app.post('/users/login', (req,res) =>{
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) =>{
+        user.generateAuthToken().then((token) =>{
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) =>{
+        res.status(400).send();
+    });
+})
+
+
 app.listen(port, ()=>{
     console.log(`Started on port ${port}`);
 })
