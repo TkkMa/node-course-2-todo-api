c1b88cd0669b24c75928975f9204c79f354ef0da
Terence Ma <tma@gsma.com>
2018-03-02 13:57:29 +0800

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 var env = process.env.NODE_ENV || 'development';  // only set in Heroku, none on local machine
 //console.log('env *****', env);
 if (env === 'development' || env === 'test'){
-    //developemtna nd test file
+    //development and test variables are stored in config.json but won't be uploaded stored in gitignore
     var config = require('./config.json');
     var envConfig = config[env];
     Object.keys(envConfig).forEach((key) =>{
